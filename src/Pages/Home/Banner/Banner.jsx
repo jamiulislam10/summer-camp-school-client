689bdc3c064b2c842c134fa4870266ba3fc6e7f3
jamiulislam10 <jamiulislam156@gmail.com>
2023-06-14 20:43:18 +0600

    banner section complete

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -43,12 +43,38 @@ const Banner = () => {
                     </div>
                     <div>
                         <img src={img4} />
+                        <div className="absolute rounded-xl flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
+                <div className='text-white space-y-7 pl-12 w-1/2'>
+                    <h2 className='text-6xl font-bold'>In the summer of 1996,
+                     Georgia in the United States to play beach volleyball at the Olympic Games.. </h2>
+                    <p>It made me what I am,’he says, ‘I saw how entertaining it was for the fans. How big it was. </p>
+                  
+                </div>
+            </div>
+                    
+                        
                     </div>
                     <div>
                         <img src={img5} />
+                        <div className="absolute rounded-xl flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
+                <div className='text-white space-y-7 pl-12 w-1/2'>
+                    <h2 className='text-6xl font-bold'> Summer camp at a world-class Padukone sports  near you. </h2>
+                    <p>We offer your kids an entire summer of fun and excitement with  drills</p>
+                  
+                </div>
+            </div>
+                    
                     </div>
                     <div>
                         <img src={img6} />
+                        <div className="absolute rounded-xl flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
+                <div className='text-white space-y-7 pl-12 w-1/2'>
+                    <h2 className='text-6xl font-bold'>Football news, results, fixtures, blogs, podcasts and comment on the Premier League. </h2>
+                    <p>We offer your kids an entire summer of fun and excitement with interesting Football drills</p>
+                  
+                </div>
+            </div>
+                    
                     </div>
                 </Carousel>
          
